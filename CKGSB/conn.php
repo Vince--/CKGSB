9721f8a78fcc15952bb860fa92d760adc07ecd7a
Vince <1570375007@qq.com>
2015-07-29 16:33:56 +0800

    Update conn.php

--- CKGSB/conn.php
+++ CKGSB/conn.php
@@ -1,6 +1,6 @@
 <?php
- @$conn = mysql_connect('localhost','eeweixin','VMQKNp3ecsLKJJjZ') or die ('数据库连接失败');
-  mysql_select_db('eeweixin') or die('选择数据库失败！');
+ @$conn = mysql_connect('localhost','','') or die ('数据库连接失败');
+  mysql_select_db('') or die('选择数据库失败！');
 
   mysql_query("SET NAMES 'utf8';") or die(mysql_error());
 
